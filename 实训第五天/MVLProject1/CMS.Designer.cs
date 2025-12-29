7b9d016a52d1a2fc227313677efce56ce58becec
Maplefall2377 <1395848853@qq.com>
2025-12-29 20:08:55 +0800

    实训第五天内容，开始接触Winform

---
+++ 实训第五天/MVLProject1/CMS.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MVLProject1
+{
+    partial class CMS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CMS));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("方正小标宋简体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(49, 366);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 91);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "登录";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("方正小标宋简体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(372, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(265, 91);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "注册";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // CMS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(698, 517);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CMS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "内容管理系统";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ 实训第五天/MVLProject1/Yoyo_WebCafe.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace MVLProject1
+{
+    public partial class Yoyo_WebCafe : Form
+    {
+        public Yoyo_WebCafe()
+        {
+            InitializeComponent();
+        }
+        private bool validateInput(string userName, string userPwd)
+        {
+            if (userName.Equals(string.Empty))
+            {
+                MessageBox.Show("用户名不能为空", "提示信息", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+                textBox1.Focus();//光标停放到登录名输入框
+                return false;
+            }
+            if (userPwd.Equals(string.Empty))
+            {//省略提示密码未输入信息或者只输入空格……
+                MessageBox.Show("密码不能为空", "提示信息", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+                textBox2.Focus();
+                return false;
+            }
+            if (userPwd.Length < 6)
+            {//省略提示密码长度不能小于6位……
+                MessageBox.Show("密码长度不能小于6位", "提示信息", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+                textBox2.Focus();
+                return false;
+            }
+            //如果输入的符合要求则继续
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String username = textBox1.Text;
+            String userpwd = textBox2.Text;
+            bool checkOut = validateInput(username, userpwd);
+            if (checkOut)
+            {
+                if (username == "admin" && userpwd == "123456")
+                    MessageBox.Show("登录成功!", "提示", MessageBoxButtons.OK);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("确认取消登录？", "提示信息", MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Information);
+            if (dr == DialogResult.OK)
+            {
+                this.Close();
+            }
+        }
+    }
+}
